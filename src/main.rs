9e687ea807a795c6e813f85f023b200443b9d203
msedou <korda.daaba@gmail.com>
2026-04-03 09:04:41 +0000

    Day 1: Rust setup + basic

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+// fn main() {
+//     let name = 5;
+//     let age = 25;
+//     println!("Hello, {} year old {}", name, age);
+// }
+
+// fn main() {
+//     let x: i32 = 10;
+//     let y: f64 = 3.14;
+//     let is_active: bool = true;
+//     let letter: char = 'A';
+
+//     println!("x is {}", x);
+//     println!("y is {}", y);
+//     println!("is_active is {}", is_active);
+//     println!("letter is {}", letter);
+// }
+
+// fn main() {
+//     let mut count: i32 = 0;
+//     count = count + 1;
+//     println!("Count: {}", count);
+// }
+
+fn main() {
+    let name: &str = "Seydou";
+    let age: i32 = 25;
+    let is_active: bool = true;
+
+    println!("Name:{}", name);
+    println!("Age:{}", age);
+    println!("Active:{}", is_active);
+}
